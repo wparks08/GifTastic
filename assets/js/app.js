b286750ef82f74e96450150e04e14f41f49d5709
Will Parks <wparks08@gmail.com>
2019-10-26 16:57:39 -0700

    favorites functionality added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
 const API_KEY = "api_key=76ZP5rDu1BBh38KPtocOB2mx6rVyy6gC";
 const GIF_URL = "https://api.giphy.com/v1/gifs/search?";
+const GIF_BY_ID_URL = "https://api.giphy.com/v1/gifs/";
 const LIMIT = "&limit=10";
+
+const FAVORITES_KEY = "favorites";
 
 //Store initial button names here
 var topics = ["German Shepherd", "Puppy", "Corgi", "Husky", "Doggo", "Australian Shepherd", "Puppers", "Bork", "Heckin Cute"];
+var favorites = [];
 var currentTopic;
 var offset = 0;
+var viewingFavorites = false;
 
 //Function to render buttons in the HTML
 //Buttons should have data-name attribute matching the topic
@@ -13,6 +18,14 @@ var offset = 0;
 
 function renderButtons() {
     $("#button-display").empty();
+
+    let favoriteButton = $("<button>")
+        .addClass("btn btn-success m-1")
+        .attr("id", "favorites")
+        .html("Favorites");
+
+    $("#button-display").append(favoriteButton);
+
     topics.forEach(function (topic) {
         let btn = $("<button>")
             .attr("data-name", topic)
@@ -36,10 +49,24 @@ function getGifs() {
             renderGif(gif);
         })
     }, function (error) {
-        console.log(error);
+        console.log("Giphy API Error: " + error);
     })
 
     offset += 10;
+}
+
+//Function to get a specific gif
+
+function getGifById(id) {
+    $.ajax({
+        url: GIF_BY_ID_URL + id + "?" + API_KEY,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        renderGif(response.data);
+    }, function (error) {
+        console.log("Giphy API Error: " + error);
+    })
 }
 
 //Function to actually render the gifs
@@ -57,7 +84,17 @@ function renderGif(gif) {
         .data("gifurl", gif.images.fixed_height.url);
     staticImage.on("click", togglePlaying);
 
-    container.append([`<p>${gif.title}</p>`, staticImage, `<p>Rating: ${gif.rating}</p>`]);
+    let favoriteIcon = $("<a>")
+        .attr("href", "#")
+        .attr("data-id", gif.id)
+        .addClass("float-right favoritify")
+
+    let clearfix = $("<div>")
+        .addClass("clearfix mb-2");
+
+    toggleFavoriteStar(favoriteIcon);
+
+    container.append([`<p>${gif.title}</p>`, staticImage, clearfix, `<p class="float-left">Rating: ${gif.rating}</p>`, favoriteIcon]);
     
     $("#gif-display").append(container);
 }
@@ -76,6 +113,68 @@ function togglePlaying() {
     }
 }
 
+//Function to add or toggle a favorite star when clicked
+
+function toggleFavoriteStar(element) {
+    if (favorites.includes(element.data("id"))) {
+        fillFavoriteStar(element);
+    } else {
+        outlineFavoriteStar(element);
+    }
+}
+
+function fillFavoriteStar(element) {
+    element.empty();
+    element.append(
+        $("<i>")
+            .addClass("material-icons")
+            .text("star")
+    );
+}
+
+function outlineFavoriteStar(element) {
+    element.empty();
+    element.append(
+        $("<i>")
+            .addClass("material-icons")
+            .text("star_border")
+    );
+}
+
+//Functions to add and remove a gif id from favorites
+
+function addFavorite(id) {
+    favorites.push(id);
+    localStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
+}
+
+function removeFavorite(id) {
+    for (let i = 0; i < favorites.length; i++) {
+        if (favorites[i] === id) {
+            favorites.splice(i, 1);
+            break;
+        }
+    }
+    localStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
+}
+
+//Function to get favorites from local storage and assing to favorites array
+
+function getFavorites() {
+    if (localStorage.getItem(FAVORITES_KEY)) {
+        favorites = JSON.parse(localStorage.getItem("favorites"));
+    }
+}
+
+function displayFavorites() {
+    viewingFavorites = true;
+    $("#more").hide();
+    $("#gif-display").empty();
+    favorites.forEach(function (id) {
+        getGifById(id);
+    });
+}
+
 //Function to grab new topic from form and add it to array
 
 $(".add-button").on("click", function(event) {
@@ -90,18 +189,45 @@ $(".add-button").on("click", function(event) {
 //Click event assigned to buttons to grab specified gifs via API call
 
 $(document).on("click", ".topic", function() {
+    viewingFavorites = false;
     currentTopic = $(this).data().name;
     $("#gif-display").empty();
     offset = 0;
     getGifs();
 });
 
+//Click event to save a gif to favorites
+
+$(document).on("click", ".favoritify", function(event) {
+    event.preventDefault();
+    let id = $(this).data("id");
+    if (favorites.includes(id)) {
+        removeFavorite(id)
+    } else {
+        addFavorite(id);
+    }
+    toggleFavoriteStar($(this));
+    if (viewingFavorites) {
+        displayFavorites();
+    }
+})
+
 //Click event to load more gifs
 
 $("#more").on("click", function(event) {
     event.preventDefault();
     getGifs();
-})
+});
+
+//Click event to display favorites
+
+$(document).on("click", "#favorites", function(event) {
+    event.preventDefault();
+    displayFavorites();
+});
+
+//Initialize favorites from local storage
+getFavorites();
 
 //Call renderButtons() to initialize page
 $("#more").hide();
